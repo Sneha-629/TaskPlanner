1e46ca51094f94d75b05d99997e3d70f60b8ec2c
Sneha-629 <snehanagaraj796@gmail.com>
2024-11-28 18:49:26 +0530

    App.js file added

---
+++ App.js
@@ -0,0 +1,11 @@
+import Todolist from "./React-Todo-list-main/React-Todo-list-main/Todolist";
+
+function App() {
+  return (
+    <div className="App">
+      <Todolist/>
+    </div>
+  );
+}
+
+export default App;
